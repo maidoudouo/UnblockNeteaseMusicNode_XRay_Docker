4e378d7f2f7e9c6bf7567480fcfabf78c4344f84
maidoudouo <maidoudouo@outlook.com>
2021-06-24 15:52:11 +0800

    fix iOS can't play

--- UnblockNeteaseMusicNode/entrypoint.sh
+++ UnblockNeteaseMusicNode/entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 [ "${SOURCE}" = "" ] && SOURCE_ARG="" || SOURCE_ARG="-o"
 [ "${ENABLE_FLAC}" = "yes" ] && export ENABLE_FLAC=true
 
-node app.js -p 5200 ${SOURCE_ARG} ${SOURCE}
+node app.js -p 5200:5201 -s -e https://music.163.com ${SOURCE_ARG} ${SOURCE}
